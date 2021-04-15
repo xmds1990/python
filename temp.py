38b817b5e705313bf961f51e3311216a58297a18
xmds1990 <905565099@qq.com>
2021-04-15 16:38:34 +0800

    v1.0.0

---
+++ temp.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+"""
+print('hello world')
+#整型
+print(1+2)
+print('1+2')
+print("1+2")
+print(1.1+2.2)
+#bool类型
+print(3.14<3)
+print(4>9)
+#定义变量
+Level=1
+ATK=5
+HP=1000
+word = 'hello world'
+#输出变量
+print(Level,ATK,HP)
+print(word)
+#变量赋值给变量
+word2 = word
+print(word2)
+#input函数
+#age = input('My age is:')
+#print(age)
+#PlayerID=input('请输入你的ID：')
+#print(PlayerID)
+#字符串语法
+print('He said "Today\'s weather is good."\nHow are you')
+#索引
+string = 'hello_shiyanlou'
+#获取第六个字符
+print(string[6])
+#获取倒数第二个字符
+print(string[-2])
+
+
+name=input('请输入你的姓名：')
+age=input('请输入你的年龄')
+print('我叫'+name+',我今年'+age+'岁了')
+#占位符
+print('我叫{},我今年{}岁了'.format(name, age))
+#格式化字符串
+print("{:.2f}".format(3.1415926))
+"""
+#运算符
+a=5
+b=9
+c=14
+#算术运算符
+print(a+b)
+print(a-c)
+print(a*c)
+print(c/b)
+print(c%a)
+print(c//a)
+#比较运算符
+print(a==b)
+print(a!=c)
+print(a>c)
+print(c<b)
+print(c>=a)
+print(c<=a)
+#赋值运算符
+a += b
+b -= c
+c *= c
+print(a,b,c)
+逻辑运算符
+print(not 1)
+print(not 0)
+print(1 and 1)
+print(1 and 0)
+print(1 or 0)
